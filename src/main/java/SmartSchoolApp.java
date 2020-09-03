35e82579b4b23173d0e05c18f7f9c5168587d690
KorkmaZerOne <omervankorkmaz@gmail.com>
2020-09-03 14:16:36 +0200

    some info added...

--- src/main/java/SmartSchoolApp.java
+++ src/main/java/SmartSchoolApp.java
@@ -2,6 +2,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,15 +15,31 @@ public class SmartSchoolApp {
         Person person1 = new Person();
         person1.setFirstName("Omer");
         person1.setFamilyName("Korkmaz");
-        person1.setGender(Gender.MALE);
+        person1.setGender(Gender.AGENDER);
 
         Course course1 = new Course();
+        course1.setName("Java EEr");
+        course1.setActive(true);
+        course1.setCode("java2020");
+        course1.setDescription("Deze opleiding richt zich tot werkzoekenden met interesse in programmatie.");
+        course1.setImageUrl("http://www.intecbrussel.be/opleidingen/java-developer-1//");
+
         Course course2 = new Course();
 
         Module module1 = new Module();
+        module1.setName("Java Fundamental");
+        module1.setCurse(course1);
+        module1.setDescription("Deze opleiding richt zich tot werkzoekenden met interesse in programmatie.");
+
         Module module2 = new Module();
 
         Exam exam1 = new Exam();
+        exam1.setName("Evaluation");
+        exam1.setDate(LocalDate.now());
+        exam1.setDescription("No comment");
+        exam1.setModule(module1);
+        exam1.setWeight(50);
+        exam1.setTotal(100);
 
         Grade grade1 = new Grade();
         grade1.setName("Weak");
@@ -36,8 +53,15 @@ public class SmartSchoolApp {
         user1.setActive(true);
 
         List<Course> courses = new ArrayList<Course>();
+        courses.add(course1);
+        courses.add(course2);
+
         List<Module> modules = new ArrayList<Module>();
+        modules.add(module1);
+        modules.add(module2);
+
         List<Exam> exams = new ArrayList<Exam>();
+
         List<Grade> grades = new ArrayList<Grade>();
         grades.add(grade1);
 
@@ -54,6 +78,5 @@ public class SmartSchoolApp {
 
         em.close();
         emf.close();
-
     }
 }
